54b912f35801e71e1677574822c1ee1e81d8daf3
Acretock <georgoncharov@gmail.com>
2021-04-20 19:15:13 +0500

    v1.0

--- Week3-1-1/Week3-1-1.cpp
+++ Week3-1-1/Week3-1-1.cpp
@@ -1,20 +1,21 @@
-// Week3-1-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <vector>
+#include <algorithm>
 #include <iostream>
+#include <cmath>
+using namespace std;
+
+bool mod(int a, int b) { return (abs(a) < abs(b)); }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<int> arr;
+	int n, tmp;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		arr.push_back(tmp);
+	}
+	sort(begin(arr), end(arr), mod);
+	for (auto i : arr)
+		cout << i << ' ';
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
